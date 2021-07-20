ffc0b2adf659e4a4e2fd95e6c5fa194402985dc1
Karen Wals <josefwalls@Karens-MacBook-Air.local>
2021-07-19 23:21:54 -0500

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import "./Header.css";
+import {Link} from 'react-router-dom';
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+
+function Header(){
+    return (
+        <nav className="header">
+            <Link to="/">
+                <img className="header__logo" src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"></img>
+            </Link>
+            <div className="header__search">
+                <input className="header__searchInput" type="text"/>
+                <SearchIcon className="header_searchIcon"/>
+            </div>
+            <div className="header__nav">
+                <Link to="/login" className="header__link">
+                    <div className="header__options">
+                        <span className="header__optionLineOne">Hello Josef</span>
+                        <span className="header__optionLineTwo">Sign in</span>
+                    </div>
+                </Link>
+                <Link to="/" className="header__link">
+                    <div className="header__options">
+                        <span className="header__optionLineOne">Returns</span>
+                        <span className="header__optionLineTwo">& Orders</span>
+                    </div>
+                </Link>
+                <Link to="/" className="header__link">
+                    <div className="header__options">
+                        <span className="header__optionLineOne">Your</span>
+                        <span className="header__optionLineTwo">Prime</span>
+                    </div>
+                </Link>
+                <Link to="/checkout" className="header__link">
+                    <div className="header__optionBasket">
+                        <ShoppingBasketIcon/>
+                        <span className="header__optionLineTwo header__basketCount">0</span>
+                    </div>
+                </Link>
+            </div>
+        </nav>
+    )
+}
+
+export default Header;
---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./Home.css"
+
+function Home(){
+    return (
+        <div className="home">
+            <img className="home__image" src="https://images-na.ssl-images-amazon.com/images/G/01/digital/video/merch/2020/Other/TWAR_2021_UPDATED_1500x600_GlowChrisSoloNoGun_TH_Post_1PV_en-US_ARSDE_1Movie._CB664039800_.jpg" alt=""/>
+        </div>
+    )
+}
+
+
+export default Home;
